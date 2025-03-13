f532bf2f9ef00e18450e704cc10ab3f927616629
Esdras Castro <ecff.dev@gmail.com>
2025-03-13 10:37:56 -0300

    feat: add helper functions

--- app/_lib/utils.ts
+++ app/_lib/utils.ts
@@ -1,6 +1,23 @@
-import { type ClassValue,clsx } from 'clsx'
+import { type ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function convertFromHundredUnitsToAmount(value: number) {
+  return value / 100
+}
+
+export function convertFromAmountToHundredUnits(value: number) {
+  return value * 100
+}
+
+export function formatCurrency(value: number) {
+  const amount = new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  })
+
+  return amount.format(value)
 }
