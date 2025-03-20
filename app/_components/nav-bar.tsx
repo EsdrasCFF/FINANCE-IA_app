d4c38ace296b354ae2752d76fca73eb21d955e4f
Esdras Castro <ecff.dev@gmail.com>
2025-03-20 10:50:20 -0300

    feat: add NavBar component

---
+++ app/_components/nav-bar.tsx
@@ -0,0 +1,46 @@
+'use client'
+
+import { UserButton } from '@clerk/nextjs'
+import Image from 'next/image'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+export function NavBar() {
+  const pathname = usePathname()
+
+  const links = [
+    {
+      href: '/',
+      label: 'Dashboard',
+    },
+    {
+      href: '/transactions',
+      label: 'Transações',
+    },
+    {
+      href: '/subscription',
+      label: 'Assinatura',
+    },
+  ]
+
+  return (
+    <nav className="mx-8 my-5 flex justify-between">
+      {/* LEFT SIDE */}
+      <div className="flex items-center gap-12">
+        <Image src="/logo.svg" width={173} height={39} alt="Logo" />
+        {links.map((link) => (
+          <Link
+            href={link.href}
+            key={link.href}
+            className={pathname == link.href ? 'font-bold text-primary' : 'text-muted-foreground'}
+          >
+            {link.label}
+          </Link>
+        ))}
+      </div>
+
+      {/* RIGHT SIDE */}
+      <UserButton showName />
+    </nav>
+  )
+}
