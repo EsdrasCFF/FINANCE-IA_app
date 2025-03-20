c6247c8495fbdb6abc5e425898ae86171ac74aca
Esdras Castro <ecff.dev@gmail.com>
2025-03-20 11:33:51 -0300

    feat: add protection in route

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,6 +1,14 @@
+import { auth } from '@clerk/nextjs/server'
+import { redirect } from 'next/navigation'
+
 import { NavBar } from '../_components/nav-bar'
 
-export default function SubscriptionPage() {
+export default async function SubscriptionPage() {
+  const { userId } = await auth()
+
+  if (!userId) {
+    return redirect('/login')
+  }
   return (
     <>
       <NavBar />
