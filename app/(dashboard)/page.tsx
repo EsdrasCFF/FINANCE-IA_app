80b5956d7047d294025cfab01e2accea152dee32
Esdras Castro <ecff.dev@gmail.com>
2025-03-20 13:55:43 -0300

    feat: ensure SummaryCard is shown on Dashboard page

---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,25 @@
+import { auth } from '@clerk/nextjs/server'
+import { redirect } from 'next/navigation'
+
+import { NavBar } from '../_components/nav-bar'
+import { db } from '../_lib/prisma'
+import { SummaryCards } from './_components/summary-cards'
+
+export default async function Home() {
+  const { userId } = await auth()
+
+  if (!userId) {
+    return redirect('/login')
+  }
+
+  const categories = await db.category.findMany({ where: { userId } })
+
+  return (
+    <>
+      <NavBar />
+      <div className="flex w-full px-6">
+        <SummaryCards categories={categories} />
+      </div>
+    </>
+  )
+}
