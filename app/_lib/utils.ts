78629d73561c5f55238e8c503ca235c35a1be720
Esdras Castro <ecff.dev@gmail.com>
2025-03-21 08:21:23 -0300

    feat: add generateMonths helper function

--- app/_lib/utils.ts
+++ app/_lib/utils.ts
@@ -1,5 +1,5 @@
 import { type ClassValue, clsx } from 'clsx'
-import { format } from 'date-fns'
+import { format, subMonths } from 'date-fns'
 import { ptBR } from 'date-fns/locale'
 import { twMerge } from 'tailwind-merge'
 
@@ -27,4 +27,44 @@ export function formatCurrency(value: number) {
 export function formatDateToCompleteLabel(date: Date) {
   const newDate = format(date, "dd 'de' MMMM 'de' yyyy", { locale: ptBR })
   return newDate
+}
+
+export function generateMonths(distanteInMonth: number) {
+  if (distanteInMonth > -12 && distanteInMonth < 0) {
+    const distance = distanteInMonth * -1
+
+    const months = Array.from({ length: 11 + distance }, (_, i) => {
+      const date = subMonths(new Date(), i)
+      return {
+        value: format(date, 'yyyy-MM'), // Exemplo: "2025-03"
+        label: format(date, "MMMM'/'yy", { locale: ptBR }),
+      }
+    })
+
+    return months
+  }
+
+  if (distanteInMonth < -12) {
+    const distance = distanteInMonth * -1 + 6
+
+    const months = Array.from({ length: distance }, (_, i) => {
+      const date = subMonths(new Date(), i)
+      return {
+        value: format(date, 'yyyy-MM'), // Exemplo: "2025-03"
+        label: format(date, "MMMM'/'yy", { locale: ptBR }),
+      }
+    })
+
+    return months
+  }
+
+  const generateMonths = Array.from({ length: 12 }, (_, i) => {
+    const date = subMonths(new Date(), i)
+    return {
+      value: format(date, 'yyyy-MM'), // Exemplo: "2025-03"
+      label: format(date, "MMMM'/'yy", { locale: ptBR }), // Exemplo: "Março/25"
+    }
+  })
+
+  return generateMonths
 }
