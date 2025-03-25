489f00d36ab5f0e18d942b29de2b7351e5b51d0f
Esdras Castro <ecff.dev@gmail.com>
2025-03-24 22:20:26 -0300

    feat: add new link on Nav

--- app/_components/nav-bar.tsx
+++ app/_components/nav-bar.tsx
@@ -21,10 +21,14 @@ export function NavBar() {
       href: '/subscription',
       label: 'Assinatura',
     },
+    {
+      href: '/categories',
+      label: 'Categorias',
+    },
   ]
 
   return (
-    <nav className="mx-8 my-5 flex justify-between">
+    <nav className="mx-8 flex h-full max-h-[72px] w-full items-center justify-between border-b px-10 py-4">
       {/* LEFT SIDE */}
       <div className="flex items-center gap-12">
         <Link href="/">
