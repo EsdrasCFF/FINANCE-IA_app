bf41752765d78d0464d639932af45cb1c65ab0b4
Esdras Castro <ecff.dev@gmail.com>
2025-03-26 15:25:25 -0300

    refactor: move validation schemas to separate file following Clean Architecture

---
+++ app/_validation/transactions/edit-transaction-validator.ts
@@ -0,0 +1,17 @@
+import { PaymentMethod,TransactionType } from '@prisma/client'
+import { z } from 'zod'
+
+export const editTransactionFormSchema = z.object({
+  name: z.string().trim().min(3, { message: 'O nome é obrigatório!' }),
+  amount: z
+    .number({ required_error: 'Valor é obrigatório!' })
+    .positive({ message: 'O valor precisa ser positivo' }),
+  type: z.nativeEnum(TransactionType),
+  category: z.string({ required_error: '' }).trim().min(3, { message: 'Categoria é obrigatório!' }),
+  paymentMethod: z.nativeEnum(PaymentMethod, {
+    required_error: 'Método de pagamento é obrigatório',
+  }),
+  date: z.coerce.date({ required_error: 'Data é obrigatória!' }),
+})
+
+export type EditTransactionFormData = z.infer<typeof editTransactionFormSchema>
