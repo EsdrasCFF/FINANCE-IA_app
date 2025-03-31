36349ef460bcc8f37a6fff89cb5ce875ef060f8e
Esdras Castro <ecff.dev@gmail.com>
2025-03-31 11:38:56 -0300

    feat: ensure categories is updated as soon as it is created

---
+++ app/_features/categories/components/main-area.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import { DataTable } from '@/app/_components/ui/data-table'
+import { categorieColumns } from '@/app/categories/_columns'
+
+import { useGetCategories } from '../api/use-get-categories'
+
+export function MainArea() {
+  const getCategories = useGetCategories()
+
+  const categories = getCategories.data?.categories || []
+
+  const isLoading = getCategories.isLoading
+
+  return (
+    <main className="h-full w-full space-y-6">
+      <DataTable columns={categorieColumns} data={categories} isLoading={isLoading} />
+    </main>
+  )
+}
--- app/categories/page.tsx
+++ app/categories/page.tsx
@@ -1,10 +1,8 @@
 import { auth } from '@clerk/nextjs/server'
 import { redirect } from 'next/navigation'
 
-import { DataTable } from '../_components/ui/data-table'
 import { AddCategoryButton } from '../_features/categories/components/add-category-button'
-import { db } from '../_lib/prisma'
-import { categorieColumns } from './_columns'
+import { MainArea } from '../_features/categories/components/main-area'
 
 export default async function CategoriesPage() {
   const { userId } = await auth()
@@ -13,16 +11,14 @@ export default async function CategoriesPage() {
     return redirect('/login')
   }
 
-  const categories = await db.category.findMany({ where: { userId } })
-
   return (
     <div className="space-y-6 px-6 pb-6">
       <div className="mt-5 flex w-full items-center justify-between">
         <h1 className="text-2xl font-bold">Categorias</h1>
-        <AddCategoryButton categories={categories} />
+        <AddCategoryButton />
       </div>
 
-      <DataTable columns={categorieColumns} data={categories} />
+      <MainArea />
     </div>
   )
 }
