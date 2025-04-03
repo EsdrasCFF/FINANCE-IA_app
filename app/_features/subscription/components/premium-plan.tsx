1ade854d1a14119001e4f31658671f96874a3f4e
Esdras Castro <ecff.dev@gmail.com>
2025-04-03 13:18:25 -0300

    feat: emsure user is redirected to stripe checkout when paying plan premium

--- app/_features/subscription/components/premium-plan.tsx
+++ app/_features/subscription/components/premium-plan.tsx
@@ -1,10 +1,27 @@
+'use client'
+
+import { loadStripe } from '@stripe/stripe-js'
 import { Check } from 'lucide-react'
 
 import { Button } from '@/app/_components/ui/button'
 import { Card } from '@/app/_components/ui/card'
 import { Separator } from '@/app/_components/ui/separator'
 
+import { crateStripeCheckout } from '../actions/create-stripe-checkout'
+
 export function PremiumPlan() {
+  async function handleBuyPremiumPlanClick() {
+    const { sessionId } = await crateStripeCheckout()
+
+    const stripe = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string)
+
+    if (!stripe) {
+      throw new Error('Stripe not found')
+    }
+
+    await stripe.redirectToCheckout({ sessionId })
+  }
+
   return (
     <Card className="relative flex w-full max-w-[450px] flex-col py-10">
       {/* PRINCE AND DESCRIPTION */}
@@ -29,7 +46,9 @@ export function PremiumPlan() {
           <Check className="text-primary" /> <span>...</span>
         </div>
 
-        <Button className="mt-3 rounded-full">Adquirir Pro</Button>
+        <Button className="mt-3 rounded-full" onClick={handleBuyPremiumPlanClick}>
+          Adquirir Pro
+        </Button>
       </div>
 
       {/* <Badge className="absolute left-10 top-10 w-fit bg-primary/20 px-4 text-base font-semibold text-primary">
